7c180066ca0d3d87c1ab6c61c40e203b65125046
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-13 10:12:34 +0530

    null, safe calls and elvis operator

--- src/AMS.kt
+++ src/AMS.kt
@@ -2,32 +2,28 @@ import java.util.*
 
 fun main(args: Array<String>) {
 
-    print(
-        "Good ${if (args[1].toInt() < 12)  "morning" else "night"} ${args[0]}, "
+    for (i in 1..10 ) {
+        val fortune = getFortuneCookie()
+        println("Your fortune is: ${fortune}")
+
+        if (fortune.contains("Take it easy")) break
+    }
+}
+
+fun getFortuneCookie(): String {
+
+    val fortunes = listOf(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune.",
     )
 
-    queryDayOfWeek(args[0])
+    print("Enter your birthday: ")
+    val birthday = readLine() ?.toIntOrNull() ?: 1
 
-}
-
-fun queryDayOfWeek(name: String) {
-
-    println("What day is today?")
-    println("Today is " + dayOfWeek())
-
-}
-
-fun dayOfWeek(): String {
-
-    return when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
-        1 -> "Sunday"
-        2 -> "Monday"
-        3 -> "Tuesday"
-        4 -> "Wedensday"
-        5 -> "Thursday"
-        6 -> "Friday"
-        7 -> "Saturday"
-        else -> "Misterious Day"
-    }
-
+    return fortunes[birthday % fortunes.size]
 }
