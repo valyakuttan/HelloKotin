d4661e774024f47068e72e36215c8455f766cd45
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-13 21:46:53 +0530

    random number and the use of when

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,7 +1,23 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    for (arg in args)
-        println(arg)
+    feedFish()
+}
 
+fun feedFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = listOf("Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday")
+
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood(day: String) = when (day) {
+    "Monday" -> "flakes"
+    else -> "pellets"
 }
