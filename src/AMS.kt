f2710542c469179ba891f83089d7f870ead127fa
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-13 22:03:03 +0530

    default values to function parameters

--- src/AMS.kt
+++ src/AMS.kt
@@ -4,10 +4,20 @@ fun main(args: Array<String>) {
     feedFish()
 }
 
+fun shouldChangeWater(
+    day: String,
+    dirty: Int = 20,
+    temperature: Int = 22
+): Boolean {
+    return true
+}
+
 fun feedFish() {
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
+
+    if (shouldChangeWater(day)) println("Change the water today.")
 }
 
 fun randomDay(): String {
